229ae1235bd14cd1ba68a2d60ca8868cd3988e8c
bg-shorthand <hanhanhan333@gmail.com>
2021-03-08 13:54:43 +0900

    0308

---
+++ 0308/react-build-system-practice/src/index.js
@@ -0,0 +1,17 @@
+import { createElement as h } from 'react'
+import { render } from 'react-dom'
+
+const iconElement = h('img', { key: 'child-asdf2', alt: '', src: '/assets/icons/spinner.svg', height: 12 })
+const buttonElement = h('button', {
+  type: 'button',
+  className: 'button button__upload button--pending',
+  onClick(e) {
+    console.log(e.target);
+  },
+  },
+  '업로드 중',
+  iconElement
+)
+
+const rootNode = document.getElementById('root');
+render(buttonElement, rootNode);
